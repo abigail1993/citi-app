51ef26ecf3743b24171c4e04833b5ea9ce21252a
abigail <alex.abi.roja@gmail.com>
2018-04-24 04:20:28 -0500

    maquetado simple

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("form");
+let array = Array.from(form)
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  if (validate(form)===true) {
+    console.log("datos válido... enviar...");
+  } else {
+    console.log("datos inválidos");
+  }
+});
+
+function validate(element) {
+  //escribe tu código aqui
+  let total = array.map((uno) => {
+    return uno.value;// esto muestra un array de los datos que se metieron en los input
+    console.log(uno.value)
+  });
+/*  total.forEach((inputValidate) => {
++})*/
+
+
+
+let userName = document.getElementById('user').value;
+let pass = document.getElementById('pass').value;
+ console.log(userName);
+}
